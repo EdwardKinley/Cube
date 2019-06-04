d1c5acab481dcdb4bfacb5b384a4c51790899e18
EdwardKinley <edward_kinley@yahoo.co.uk>
2019-06-05 00:23:04 +0100

    sets up cube square-showing spaces

--- js/cube.js
+++ js/cube.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function setUpCubeSpace(size) {
     main.innerHTML = '';
-    // main.style.backgroundColor = 'grey';
+    main.style.backgroundColor = 'grey';
     for (i=0; i<3; i++) {
       const cubeRow = document.createElement('div');
       cubeRow.className = 'cubeRow';
@@ -63,12 +63,37 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function showCube(size) {
-    console.log('about to show cube with size', size);
+    // console.log('about to show cube with size', size);
     const faceSpaceIDs = [12, 21, 22, 23, 24, 32];
     for (i=0; i<faceSpaceIDs.length; i++) {
       const faceSpace = document.querySelector(`#faceSpace${faceSpaceIDs[i]}`);
-      faceSpace.textContent = 'here';
-      faceSpace.style.backgroundColor = 'black';
+      faceSpace.style.backgroundColor = 'grey';
+      for (j=0; j<size+2; j++) {
+        const faceRow = document.createElement('div');
+        faceRow.className = 'faceRow';
+        if (j==0 || j==size+1) {
+          faceRow.style.height = '14%';
+          faceRow.style.backgroundColor = 'salmon';
+        } else {
+          faceRow.style.height = `${72/size}%`;
+          const randomNumber = Math.floor(Math.random() * 6);
+          faceRow.style.backgroundColor = `${colours[randomNumber]}`;
+        }
+        faceSpace.appendChild(faceRow);
+        for (k=0; k<size+2; k++) {
+          const faceSquare = document.createElement('div');
+          faceSquare.className = 'faceSquare';
+          if (k==0 || k==size+1) {
+            faceSquare.style.width = '14%';
+            faceSquare.style.backgroundColor = 'salmon';
+          } else if (j!=0 && j!=size+1) {
+            faceSquare.style.width = `${72/size}%`;
+            const randomNumber = Math.floor(Math.random() * 6);
+            faceSquare.style.backgroundColor = `${colours[randomNumber]}`;
+          }
+          faceRow.appendChild(faceSquare);
+        }
+      }
     }
   }
 
