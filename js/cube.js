0c21105697086d9e4602ddc49e9a465dadb5f1ad
EdwardKinley <edward_kinley@yahoo.co.uk>
2019-06-05 22:37:20 +0100

    shows initial colours

--- js/cube.js
+++ js/cube.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   main = document.querySelector('.main');
   cubeSizes = [2, 3, 4, 5
-    , 6, 7, 8, 9
+    // , 6, 7, 8, 9
   ];
-  colours = ['white', 'green', 'blue', 'red', 'darkorange', 'yellow'];
+  colours = ['darkorange', 'green', 'white', 'blue', 'yellow', 'red'];
 
   showCubeSizeOptions();
 
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function setUpCubeSpace(size) {
     main.innerHTML = '';
-    main.style.backgroundColor = 'grey';
+    // main.style.backgroundColor = 'grey';
     for (i=0; i<3; i++) {
       const cubeRow = document.createElement('div');
       cubeRow.className = 'cubeRow';
@@ -67,17 +67,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const faceSpaceIDs = [12, 21, 22, 23, 24, 32];
     for (i=0; i<faceSpaceIDs.length; i++) {
       const faceSpace = document.querySelector(`#faceSpace${faceSpaceIDs[i]}`);
-      faceSpace.style.backgroundColor = 'grey';
+      // faceSpace.style.backgroundColor = 'black';
       for (j=0; j<size+2; j++) {
         const faceRow = document.createElement('div');
         faceRow.className = 'faceRow';
         if (j==0 || j==size+1) {
           faceRow.style.height = '14%';
-          faceRow.style.backgroundColor = 'salmon';
+          // faceRow.style.backgroundColor = 'salmon';
         } else {
           faceRow.style.height = `${72/size}%`;
-          const randomNumber = Math.floor(Math.random() * 6);
-          faceRow.style.backgroundColor = `${colours[randomNumber]}`;
+          // const randomNumber = Math.floor(Math.random() * 6);
+          // faceRow.style.backgroundColor = `${colours[randomNumber]}`;
         }
         faceSpace.appendChild(faceRow);
         for (k=0; k<size+2; k++) {
@@ -85,11 +85,23 @@ document.addEventListener('DOMContentLoaded', () => {
           faceSquare.className = 'faceSquare';
           if (k==0 || k==size+1) {
             faceSquare.style.width = '14%';
-            faceSquare.style.backgroundColor = 'salmon';
-          } else if (j!=0 && j!=size+1) {
+          } else {
             faceSquare.style.width = `${72/size}%`;
-            const randomNumber = Math.floor(Math.random() * 6);
-            faceSquare.style.backgroundColor = `${colours[randomNumber]}`;
+          }
+          if (k==0 || k==size+1) {
+            // faceSquare.style.backgroundColor = 'salmon';
+            const buttonSpace = document.createElement('button');
+            buttonSpace.className = 'buttonSpace';
+            buttonSpace.textContent = 'b';
+            faceSquare.appendChild(buttonSpace);
+          } else if (j!=0 && j!=size+1) {
+            const faceSticker = document.createElement('div');
+            faceSticker.className = 'faceSticker';
+            faceSquare.style.backgroundColor = 'black';
+            faceSquare.appendChild(faceSticker);
+            // const randomNumber = Math.floor(Math.random() * 6);
+            // faceSticker.style.backgroundColor = `${colours[randomNumber]}`;
+            faceSticker.style.backgroundColor = `${colours[i]}`;
           }
           faceRow.appendChild(faceSquare);
         }
