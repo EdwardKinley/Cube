db35924627c7fc5a59c3a31938ebd4280f35dfcf
EdwardKinley <edward_kinley@yahoo.co.uk>
2019-06-07 11:09:38 +0100

    enables moves

--- js/cube.js
+++ js/cube.js
@@ -102,6 +102,7 @@ document.addEventListener('DOMContentLoaded', () => {
           } else if (j!=0 && j!=size+1) {
             const faceSticker = document.createElement('div');
             faceSticker.className = 'faceSticker';
+            faceSticker.id = `sticker${i+1}${j}${k}`;
             faceSquare.style.backgroundColor = 'black';
             faceSquare.appendChild(faceSticker);
             // const randomNumber = Math.floor(Math.random() * 6);
@@ -125,9 +126,172 @@ document.addEventListener('DOMContentLoaded', () => {
     if (moveButton.id[6] == chosenSize+1) {  moveButton.textContent = '▶'; }
     faceSquare.appendChild(moveButton);
     moveButton.addEventListener('click', () => {
-      console.log(moveButton.id);
+      move(i, j, k);
     })
+  }
+
+  function move(i, j, k) {
+    console.log('i', i, 'j', j, 'k', k);
+    face = i+1;
+    s = chosenSize + 1;
+    moves = [];
+    toSwivel = 0;
+    // console.log(toSwivel);
+    fjk = face.toString()+j.toString()+k.toString();
+    // console.log(fjk);
+
+    if (fjk == 110 || fjk == `2${s}1` || fjk == `6${s-1}${s}` || fjk == `40${s-1}`) { toSwivel = '5c'; }
+    if (fjk == `11${s}` || fjk == `4${s}${s-1}` || fjk == `6${s-1}0` || fjk == 201) { toSwivel = '5a'; }
+    if (fjk == `1${s-1}0` || fjk == `2${s}${s-1}` || fjk == `61${s}` || fjk == 401) { toSwivel = '3a'; }
+    if (fjk == `1${s-1}${s}` || fjk == `4${s}1` || fjk == 610 || fjk == `20${s-1}`) { toSwivel = '3c'; }
+    if (fjk == 101 || fjk == `5${s}${s-1}` || fjk == 601 || fjk == 301) { toSwivel = '2a'; }
+    if (fjk == `1${s}1` || fjk == `3${s}1` || fjk == `6${s}1` || fjk == `50${s-1}`) { toSwivel = '2c'; }
+    if (fjk == `10${s-1}` || fjk == `5${s}1` || fjk == `60${s-1}` || fjk == `30${s-1}`) { toSwivel = '4c'; }
+    if (fjk == `1${s}${s-1}` || fjk == `3${s}${s-1}` || fjk == `6${s}${s-1}` || fjk == 501) { toSwivel = '4a'; }
+    if (fjk == 310 || fjk == 210 || fjk == 510 || fjk == 410) { toSwivel = '1c'; }
+    if (fjk == `31${s}` || fjk == `41${s}` || fjk == `51${s}` || fjk == `21${s}`) { toSwivel = '1a'; }
+    if (fjk == `3${s-1}0` || fjk == `2${s-1}0` || fjk == `5${s-1}0` || fjk == `4${s-1}0`) { toSwivel = '6a'; }
+    if (fjk == `3${s-1}${s}` || fjk == `4${s-1}${s}` || fjk == `5${s-1}${s}` || fjk == `2${s-1}${s}`) { toSwivel = '6c'; }
+    // console.log(toSwivel);
+
+    if (face == 1) {
+      if (k == 0) { moves = [`1${j}l`, `2${j}d`, `6${s-j}r`, `4${s-j}u`]; }
+      if (k == s) { moves = [`1${j}r`, `4${s-j}d`, `6${s-j}l`, `2${j}u`]; }
+      if (j == 0) { moves = [`1${k}u`, `5${s-k}d`, `6${k}u`, `3${k}u`]; }
+      if (j == s) { moves = [`1${k}d`, `3${k}d`, `6${k}d`, `5${s-k}u`]; }
+    }
+    if (face == 2) {
+      if (k == 0) { moves = [`2${j}l`, `5${j}l`, `4${j}l`, `3${j}l`]; }
+      if (k == s) { moves = [`2${j}r`, `3${j}r`, `4${j}r`, `5${j}r`]; }
+      if (j == 0) { moves = [`2${k}u`, `1${k}r`, `4${s-k}d`, `6${s-k}l`]; }
+      if (j == s) { moves = [`2${k}d`, `6${s-k}r`, `4${s-k}u`, `1${k}l`]; }
+    }
+    if (face == 3) {
+      if (k == 0) { moves = [`3${j}l`, `2${j}l`, `5${j}l`, `4${j}l`]; }
+      if (k == s) { moves = [`3${j}r`, `4${j}r`, `5${j}r`, `2${j}r`]; }
+      if (j == 0) { moves = [`3${k}u`, `1${k}u`, `5${s-k}d`, `6${k}u`]; }
+      if (j == s) { moves = [`3${k}d`, `6${k}d`, `5${s-k}u`, `1${k}d`]; }
+    }
+    if (face == 4) {
+      if (k == 0) { moves = [`4${j}l`, `3${j}l`, `2${j}l`, `5${j}l`]; }
+      if (k == s) { moves = [`4${j}r`, `5${j}r`, `2${j}r`, `3${j}r`]; }
+      if (j == 0) { moves = [`4${k}u`, `1${s-k}l`, `2${s-k}d`, `6${k}r`]; }
+      if (j == s) { moves = [`4${k}d`, `6${k}l`, `2${s-k}u`, `1${s-k}r`]; }
+    }
+    if (face == 5) {
+      if (k == 0) { moves = [`5${j}l`, `4${j}l`, `3${j}l`, `2${j}l`]; }
+      if (k == s) { moves = [`5${j}r`, `2${j}r`, `3${j}r`, `4${j}r`]; }
+      if (j == 0) { moves = [`5${k}u`, `1${s-k}d`, `3${s-k}d`, `6${s-k}d`]; }
+      if (j == s) { moves = [`5${k}d`, `6${s-k}u`, `3${s-k}u`, `1${s-k}u`]; }
+    }
+    if (face == 6) {
+      if (k == 0) { moves = [`6${j}l`, `2${s-j}u`, `1${s-j}r`, `4${j}d`]; }
+      if (k == s) { moves = [`6${j}r`, `4${j}u`, `1${s-j}l`, `2${s-j}d`]; }
+      if (j == 0) { moves = [`6${k}u`, `3${k}u`, `1${k}u`, `5${s-k}d`]; }
+      if (j == s) { moves = [`6${k}d`, `5${s-k}u`, `1${k}d`, `3${k}d`]; }
+    }
+
+    temp = [];
+    for (m=1; m<s; m++) {
+      if (moves[0][2] == 'r') { temp.push(document.querySelector(`#sticker${face}${j}${s-m}`).style.backgroundColor); }
+      if (moves[0][2] == 'l') { temp.push(document.querySelector(`#sticker${face}${j}${m}`).style.backgroundColor); }
+      if (moves[0][2] == 'd') { temp.push(document.querySelector(`#sticker${face}${s-m}${k}`).style.backgroundColor); }
+      if (moves[0][2] == 'u') { temp.push(document.querySelector(`#sticker${face}${m}${k}`).style.backgroundColor); }
+    }
+
+    const moveElements = [];
+    // console.log(moveElements);
+    for (n=0; n<moves.length; n++) {
+      // console.log(n);
+      // console.log(moves[n]);
+      const movesTemp = [];
+      for (p=0; p<chosenSize; p++) {
+        // movesTemp.push(document.querySelector(`#sticker${face}${j}${m}`));
+        // if (moves[n][2] == 'r' || moves[n][2] == 'l') { movesTemp.push(document.querySelector(`#sticker${moves[n][0]}${moves[n][1]}${p+1}`)); }
+        if (moves[n][2] == 'r') { movesTemp.push(document.querySelector(`#sticker${moves[n][0]}${moves[n][1]}${s-1-p}`)); }
+        if (moves[n][2] == 'l') { movesTemp.push(document.querySelector(`#sticker${moves[n][0]}${moves[n][1]}${p+1}`)); }
+        // if (moves[n][2] == 'd' || moves[n][2] == 'u') { movesTemp.push(document.querySelector(`#sticker${moves[n][0]}${p+1}${moves[n][1]}`)); }
+        if (moves[n][2] == 'd') { movesTemp.push(document.querySelector(`#sticker${moves[n][0]}${s-1-p}${moves[n][1]}`)); }
+        if (moves[n][2] == 'u') { movesTemp.push(document.querySelector(`#sticker${moves[n][0]}${p+1}${moves[n][1]}`)); }
+        // if (moves[n][2] == 'u' || moves[n][2] == 'd') { movesTemp.push(document.querySelector(`#sticker${face}${m}${j}`).style.backgroundColor); }
+      }
+      moveElements.push(movesTemp);
+      // console.log(movesTemp);
+    }
+
+    // console.log(moveElements);
+
+    for (r=0; r<chosenSize; r++) {
+      moveElements[0][r].style.backgroundColor = `${moveElements[moveElements.length-1][r].style.backgroundColor}`;
+    }
+
+    for (q=moveElements.length-1; q>1; q--) {
+      for (r=0; r<chosenSize; r++) {
+        moveElements[q][r].style.backgroundColor = `${moveElements[q-1][r].style.backgroundColor}`;
+      }
+    }
+
+    // console.log(temp);
+    for (r=0; r<chosenSize; r++) {
+      moveElements[1][r].style.backgroundColor = `${temp[r]}`;
+    }
+
+    // console.log(toSwivel);
+    //
+    // console.log(toSwivel[1]);
+
+    // if (toSwivel[1] == 'c') {
+    //   console.log('clockwise');
+    // }
+    //
+    //
+    // if (toSwivel[1] == 'a') {
+    //   console.log('anti');
+    // }
+    console.log(toSwivel);
+    swivel(toSwivel);
 
   }
+
+  function swivel(toSwivel) {
+    if (toSwivel[1] == 'c') {
+      const temp = [];
+      for (i=0; i<chosenSize-1; i++) { temp.push(document.querySelector(`#sticker${toSwivel[0]}${i+1}1`).style.backgroundColor);   }
+      for (i=0; i<chosenSize-1; i++) { document.querySelector(`#sticker${toSwivel[0]}${i+1}1`).style.backgroundColor = document.querySelector(`#sticker${toSwivel[0]}${chosenSize}${i+1}`).style.backgroundColor;}
+      for (i=0; i<chosenSize-1; i++) { document.querySelector(`#sticker${toSwivel[0]}${chosenSize}${i+1}`).style.backgroundColor = document.querySelector(`#sticker${toSwivel[0]}${chosenSize-i}${chosenSize}`).style.backgroundColor;}
+      for (i=0; i<chosenSize-1; i++) { document.querySelector(`#sticker${toSwivel[0]}${chosenSize-i}${chosenSize}`).style.backgroundColor = document.querySelector(`#sticker${toSwivel[0]}1${chosenSize-i}`).style.backgroundColor; }
+      for (i=0; i<chosenSize-1; i++) { document.querySelector(`#sticker${toSwivel[0]}1${chosenSize-i}`).style.backgroundColor = temp[i]; }
+
+      if (chosenSize > 3) {
+        const temp = [];
+        for (i=1; i<chosenSize-2; i++) { temp.push(document.querySelector(`#sticker${toSwivel[0]}${i+1}2`).style.backgroundColor); }
+        for (i=1; i<chosenSize-2; i++) { document.querySelector(`#sticker${toSwivel[0]}${i+1}2`).style.backgroundColor = document.querySelector(`#sticker${toSwivel[0]}${chosenSize-1}${i+1}`).style.backgroundColor; }
+        for (i=1; i<chosenSize-2; i++) { document.querySelector(`#sticker${toSwivel[0]}${chosenSize-1}${i+1}`).style.backgroundColor = document.querySelector(`#sticker${toSwivel[0]}${chosenSize-i}${chosenSize-1}`).style.backgroundColor; }
+        for (i=1; i<chosenSize-2; i++) { document.querySelector(`#sticker${toSwivel[0]}${chosenSize-i}${chosenSize-1}`).style.backgroundColor = document.querySelector(`#sticker${toSwivel[0]}2${chosenSize-i}`).style.backgroundColor; }
+        for (i=1; i<chosenSize-2; i++) { document.querySelector(`#sticker${toSwivel[0]}2${chosenSize-i}`).style.backgroundColor = temp[i-1]; }
+      }
+    }
+    if (toSwivel[1] == 'a') {
+      const temp = [];
+      for (i=0; i<chosenSize-1; i++) { temp.push(document.querySelector(`#sticker${toSwivel[0]}1${i+1}`).style.backgroundColor); }
+      for (i=0; i<chosenSize-1; i++) { document.querySelector(`#sticker${toSwivel[0]}1${i+1}`).style.backgroundColor = document.querySelector(`#sticker${toSwivel[0]}${i+1}${chosenSize}`).style.backgroundColor;}
+      for (i=0; i<chosenSize-1; i++) { document.querySelector(`#sticker${toSwivel[0]}${i+1}${chosenSize}`).style.backgroundColor = document.querySelector(`#sticker${toSwivel[0]}${chosenSize}${chosenSize-i}`).style.backgroundColor;}
+      for (i=0; i<chosenSize-1; i++) { document.querySelector(`#sticker${toSwivel[0]}${chosenSize}${chosenSize-i}`).style.backgroundColor = document.querySelector(`#sticker${toSwivel[0]}${chosenSize-i}1`).style.backgroundColor; }
+      for (i=0; i<chosenSize-1; i++) { document.querySelector(`#sticker${toSwivel[0]}${chosenSize-i}1`).style.backgroundColor = temp[i]; }
+
+      if (chosenSize > 3) {
+        const temp = [];
+        for (i=1; i<chosenSize-2; i++) { temp.push(document.querySelector(`#sticker${toSwivel[0]}2${i+1}`).style.backgroundColor); }
+        for (i=1; i<chosenSize-2; i++) { document.querySelector(`#sticker${toSwivel[0]}2${i+1}`).style.backgroundColor = document.querySelector(`#sticker${toSwivel[0]}${i+1}${chosenSize-1}`).style.backgroundColor; }
+        for (i=1; i<chosenSize-2; i++) { document.querySelector(`#sticker${toSwivel[0]}${i+1}${chosenSize-1}`).style.backgroundColor = document.querySelector(`#sticker${toSwivel[0]}${chosenSize-1}${chosenSize-i}`).style.backgroundColor; }
+        for (i=1; i<chosenSize-2; i++) { document.querySelector(`#sticker${toSwivel[0]}${chosenSize-1}${chosenSize-i}`).style.backgroundColor = document.querySelector(`#sticker${toSwivel[0]}${chosenSize-i}2`).style.backgroundColor; }
+        for (i=1; i<chosenSize-2; i++) { document.querySelector(`#sticker${toSwivel[0]}${chosenSize-i}2`).style.backgroundColor = temp[i-1]; }
+      }
+    }
+
+
+  }
+
+
 
 })
